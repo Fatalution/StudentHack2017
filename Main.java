06b779d50c24b513c34d0a8389e2488f5bb5472f
GytisVaitkus <gytis585@gmail.com>
2017-03-11 03:07:31 +0000

    call for breeding method

--- Main.java
+++ Main.java
@@ -6,8 +6,26 @@ public class Main
 
     catArray[0] = new Cat(0, 1, null, true);
     catArray[1] = new Cat(1, 1, null, true);
+    numberOfCats = 2;
+
+    while(true)
+    {
+      //getting ids for first and second cats
+      firstCat = (int) (Math.random() * numberOfCats);
+      while(secondCat != firstCat)
+      {
+        secondCat = (int) (Math.random() * numberOfCats);
+      }
+
+      //getting new cat
+      catArray[numberOfCats] = firstCat.breedCat(catArray[firstCat], catArray[secondCat]);
+      numberOfCats++;
+
+      //printing breeding info
+      System.out.println("Cat " + firstCat + " with " + secondCat + " equals " + numberOfCats);
 
 
+    }
   }
   // DIFFERENCE
 }
