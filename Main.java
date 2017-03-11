535a73bc86b21975d4e2f3ec86d1ae04879eaad9
Gytis <gytis.vaitkus@student.manchester.ac.uk>
2017-03-11 10:38:52 +0000

    started GUI

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main
 {
   public static void main(String[] args)
   {
-    Game game = new Game();
-    game.runGame();
+  //  Game game = new Game();
+  //  game.runGame();
+
+    GameGUI window = new GameGUI();
+    window.setVisible(true);
   }
 }
