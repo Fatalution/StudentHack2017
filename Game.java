ae2ec04aac4c5d7721397ff9d95d7b22ee5c97fa
Ivan Romanchuk <ivan.romanchuk@student.cs.manchester.ac.uk>
2017-03-11 08:03:26 +0000

    minor text output clarifications

--- Game.java
+++ Game.java
@@ -52,13 +52,14 @@ public class Game
     numberOfCats++;
 
     //printing breeding info
-    System.out.println("Cat " + firstCatID + " with " + secondCatID + " equals " + numberOfCats);
+    System.out.println("Cat " + firstCatID + " breeded with " + secondCatID
+                     + " will result in " + numberOfCats + " number of cats");
   }
 
   private void makeKill(Cat[] catArray)
   {
     int randomCatID = (int) (Math.random() * numberOfCats);
-    System.out.println("Cat " + randomCatID + " has died");
+    System.out.println("Cat number " + randomCatID + " has died");
 
     catArray[randomCatID] = null;
 
