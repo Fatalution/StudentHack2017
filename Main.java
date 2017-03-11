960236a3a33e6b629b5cfc7576359816f88e9f05
GytisVaitkus <gytis585@gmail.com>
2017-03-11 02:48:09 +0000

    creating first cats

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
 public class Main
 {
+  public static void main(String[] args)
+  {
+    Cat[] catArray = new Cat[100];
 
+    catArray[0] = new Cat(0, 1, null, boolean true);
+    catArray[1] = new Cat(1, 1, null, boolean true);
+
+    
+  }
 }
